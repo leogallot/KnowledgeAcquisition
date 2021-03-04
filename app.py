87c16c2a9ad7c69d800bf516c57a2f807d6b759e
Benjamin <benlemt@gmail.com>
2021-03-04 15:24:33 +0100

    Add scraper

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+from scraper.ScraperManager import ScraperManager
+
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return 'Hello world'
+    if request.method == 'POST' and request.form.get('url') != '':
+        article = ScraperManager(request.form.get('url')).get_text()
+        output = {'display': True, 'article': article, 'url': request.form.get('url')}
+        return render_template('index.html', data=output)
+    return render_template('index.html', data={'display': False})
 
 
 if __name__ == '__main__':
---
+++ scraper/ScraperManager.py
@@ -0,0 +1,14 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class ScraperManager:
+
+    def __init__(self, url):
+        self.article = requests.get(url)
+        self.soup = BeautifulSoup(self.article.content, 'html.parser')
+        self.body = self.get_body()
+
+    def get_text(self):
+        body = self.soup.find('body')
+        return [p.text for p in body.findAll('p')]
