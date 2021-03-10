31c1dad76fe0b85d482d7d595ce4685616994cdc
Benjamin <benlemt@gmail.com>
2021-03-10 15:10:24 +0100

    update top type

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 
-from engine.engine import *
+from engine.engine import Engine
 from scraper.ScraperManager import ScraperManager
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+engine = Engine()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,10 +13,14 @@ def home():
         url = request.form.get('url')
 
         article = ScraperManager(url).get_text()
-        entities_data = disambiguate(article)
+        entities_data = engine.disambiguate(article)
 
-        types = get_representative_types(entities_data)
-        images = get_entities_images(entities_data)
+        types = engine.get_representative_types(entities_data)
+
+        with open('types.json', 'a+') as file:
+            file.write(types + 5 * '\n')
+
+        images = engine.get_entities_images(entities_data)
 
         output = {'display': True, 'article': article, 'url': url, 'images': images}
         return render_template('index.html', data=output)
--- engine/engine.py
+++ engine/engine.py
@@ -8,6 +8,13 @@ class Engine:
         self.DBPEDIA_API_URL = "https://api.dbpedia-spotlight.org/en/annotate"
         self.AIDA_API_URL = "https://gate.d5.mpi-inf.mpg.de/aida/service/disambiguate"
         self.api = self.AIDA_API_URL
+        self.toptypes = {
+            'wordnet_person_': 'person',
+            'wordnet_organization_': 'organization',
+            'wordnet_event_': 'event',
+            'wordnet_artifact': 'artifact',
+            'yagoGeoEntity': 'yagogeoentity'
+        }
 
     def disambiguate(self, text):
         response = requests.post(self.api, {'text': text})
@@ -31,12 +38,28 @@ class Engine:
     def format_type(self, entity_type):
         return entity_type.replace('YAGO_', '<') + '>'
 
+    def get_top_type(self, entity_type):
+        for top_type in self.toptypes:
+            print('entity_type: ' + entity_type + ' | top type ' + top_type)
+            print(entity_type in top_type)
+            if top_type in entity_type:
+                return self.toptypes[top_type]
+        return None
+
     def get_representative_types(self, data):
         types = {}
         entities = data['entityMetadata']
         for value in entities.values():
+            # check if value is not empty
             if value:
                 entity = f'<{value["entityId"].lower()}>'
-                entity_types = list(map(self.format_type, set(value['type'])))
-                types[entity] = entity_types
+                entity_types = []
+                top_type = None
+                for entity_type in value['type']:
+                    type_formatted = self.format_type(entity_type)
+                    entity_types.append(type_formatted)
+                    if top_type is None:
+                        top_type = self.get_top_type(type_formatted)
+                types[entity] = {'type': entity_types, 'toptype': top_type}
+
         return json.dumps(types)
