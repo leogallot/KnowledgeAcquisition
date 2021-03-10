0f74483ad1d058a068d5fb90be43a90ffd2eeeb3
Benjamin <benlemt@gmail.com>
2021-03-10 13:54:18 +0100

    create class Engine

--- engine/engine.py
+++ engine/engine.py
@@ -2,42 +2,41 @@ import requests
 import re
 import json
 
-DBPEDIA_API_URL = "https://api.dbpedia-spotlight.org/en/annotate"
-AIDA_API_URL = "https://gate.d5.mpi-inf.mpg.de/aida/service/disambiguate"
 
+class Engine:
+    def __init__(self):
+        self.DBPEDIA_API_URL = "https://api.dbpedia-spotlight.org/en/annotate"
+        self.AIDA_API_URL = "https://gate.d5.mpi-inf.mpg.de/aida/service/disambiguate"
+        self.api = self.AIDA_API_URL
 
-def disambiguate(text):
-    response = requests.post(AIDA_API_URL, {'text': text})
-    return response.json()
+    def disambiguate(self, text):
+        response = requests.post(self.api, {'text': text})
+        return response.json()
 
+    def get_entities_images(self, data):
+        items = []
+        images = []
 
-def get_entities_images(data):
-    items = []
-    images = []
+        for item in data['entityMetadata']:
+            if re.search('YAGO:', item):
+                items.append(item)
 
-    for item in data['entityMetadata']:
-        if re.search('YAGO:', item):
-            items.append(item)
+        for item in items:
+            url = data['entityMetadata'][item]['depictionurl']
+            if url is not None:
+                images.append(url)
 
-    for item in items:
-        url = data['entityMetadata'][item]['depictionurl']
-        if url is not None:
-            images.append(url)
+        return images
 
-    return images
+    def format_type(self, entity_type):
+        return entity_type.replace('YAGO_', '<') + '>'
 
-
-def format_type(entity_type):
-    return entity_type.replace('YAGO_', '<') + '>'
-
-
-def get_representative_types(data):
-    types = {}
-    entities = data['entityMetadata']
-    for value in entities.values():
-        if value:
-            entity = f'<{value["entityId"].lower()}>'
-            entity_types = list(map(format_type, set(value['type'])))
-            types[entity] = entity_types
-    return json.dumps(types)
-
+    def get_representative_types(self, data):
+        types = {}
+        entities = data['entityMetadata']
+        for value in entities.values():
+            if value:
+                entity = f'<{value["entityId"].lower()}>'
+                entity_types = list(map(self.format_type, set(value['type'])))
+                types[entity] = entity_types
+        return json.dumps(types)
